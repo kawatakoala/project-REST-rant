36868eed2f265ee78fef9d180c71930acfdf2e09
Michael Kawata <michaelkawata@bens-air.lan>
2023-01-23 15:38:28 -0800

    Adding bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ const error404 = () => {
             <main>
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
+                <div>
+                    <img src="/images/shibaimg.jpg" alt="Image of Shiba" height="400" width="600" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@minhphamdesign?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Minh Pham</a> on <a href="https://unsplash.com/photos/LTQMgx8tYqM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
             </main>
         </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ const home = () => {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/images/burgerimg.jpg" alt="Image of Burger" height="400" width="600" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@eddie2oh?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Edward Franklin</a> on <a href="https://unsplash.com/s/photos/restaurant-food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                     <button className="btn-primary">Places Page</button>
                 </a>
