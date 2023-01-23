a5acaffcfa395645b61f7c176dd18415e2db8920
Michael Kawata <michaelkawata@bens-air.lan>
2023-01-22 16:37:58 -0800

    Adding 404 page view

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 app.use('/places', require('./controllers/places'))
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
