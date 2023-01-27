2bdcee50d906f6ddaa741f1122c6e72d5f6add4a
Michael Kawata <michaelkawata@bens-air.lan>
2023-01-27 14:21:23 -0800

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -5,6 +5,19 @@ router.get('/', (req, res) => {
     res.render('places/index', { places })
 })
 
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', { place: places[id] })
+    }
+})
+
 router.delete('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
