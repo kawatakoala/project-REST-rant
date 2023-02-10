8e9ea6f9634a0b7fe9500af1ad3b317c850bda8f
Michael Kawata <michaelkawata@bens-air.lan>
2023-02-10 13:09:18 -0800

    rating stars and logic  added

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,6 +7,23 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
+    if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {Math.round(averageRating)} stars
+            </h3>
+        )
+    }
+
     if (data.place.comments.length) {
         comments = data.place.comments.map(c => {
             return (
@@ -36,6 +53,7 @@ function show(data) {
                         <h2>
                             Rating
                         </h2>
+                        {rating}
                         <br />
                         <h2>
                             Description
