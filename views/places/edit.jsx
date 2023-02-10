1691e9f47a6a3aacab50a7fec2a117a5802e2ae7
Michael Kawata <michaelkawata@bens-air.lan>
2023-02-10 13:32:55 -0800

    Implement PUT and DELETE

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -53,6 +53,14 @@ function edit_form(data) {
                                 value={data.place.cuisines}
                                 required />
                         </div>
+                        <div className="form-group col-sm-4">
+                            <label htmlFor="founded">Founded</label>
+                            <input className="form-control"
+                                id="founded"
+                                name="founded"
+                                value={data.place.founded}
+                            />
+                        </div>
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
                 </form>
